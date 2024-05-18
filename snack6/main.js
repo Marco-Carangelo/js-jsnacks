92e40e9ece7c0970a9164f3a88ba181161a0599b
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-18 18:12:59 +0200

    add: input validation in snack 6

--- snack6/main.js
+++ snack6/main.js
@@ -10,14 +10,24 @@ const randomNumber = Math.floor(Math.random() * 11);
 
 const userNumber = parseInt(Number(prompt("inserire un numero da 0 a 10")))
 
-//Verifichiamo se il numero generato casualmente è uguale a quello inserito dall'utente
+//Validiamo il dato inserito dall'utente e proseguiamo solo se viene inserito un valore numerico da 0 a 10
 
-if (randomNumber === userNumber) {
+if (userNumber < 0 || userNumber > 10 || isNaN(userNumber)) {
 
-    console.log("Complimenti hai indovinato il numero vincente");
+    alert("Il valore inserito non è valido. Riprovare inserendo un numero da 0 a 10");
+
 
 } else {
+    //Verifichiamo se il numero generato casualmente è uguale a quello inserito dall'utente
 
-    console.log("Hai perso, non hai indovinato il numero vincente");
+    if (randomNumber === userNumber) {
 
+        alert("Complimenti hai indovinato il numero vincente");
+
+    } else {
+
+        alert("Hai perso, non hai indovinato il numero vincente");
+
+    }
 }
+
